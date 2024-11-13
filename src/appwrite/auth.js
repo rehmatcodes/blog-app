6b939689b0e58aab449f7bbf851fde48b129e0b0
Rehmat Ullah <your-rehmat.ullah.network@gmail.com>
2024-11-13 12:17:14 +0000

    blog app

---
+++ src/appwrite/auth.js
@@ -0,0 +1,58 @@
+import conf from "../conf.js";
+import {Client,Account,ID} from 'appwrite'
+
+export class AuthService {
+    Client  = new Client();
+    account;
+
+    constructor(){
+        this.Client
+        .setEndpoint(conf.appwriteurl)
+        .setProject(conf.appwriteprojetId);
+        this.account = new Account (this.Client);
+    }
+
+    async createAccount ({email,password,name}){
+        try {
+         const userAccount =  await this.account.create(ID.unique(),  email,password,name);
+         if (userAccount) {
+            //call another methood
+          return   this.login ({email,password});
+
+         } else {
+            return userAccount;
+         }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login ({email,password}){
+        try {
+        return  await this.account.createEmailSession(email,password);
+
+        } catch (error) {
+            throw error;
+        }
+    }
+    async getCurrentUser(){
+        try {
+             return  await  this.account.get();
+        } catch (error) {
+            console.log('appwrite service :: getcurrentuser @@ error', error);   
+        }
+        return null;
+    }
+    async logout(){
+        try {
+            await this.account.deleteSessions('current');
+        } catch (error) {
+            console.log('appwrite service :: logout::error',error);
+            
+        }
+    }
+}
+
+const authservice = new AuthService();
+
+export default authservice ;
---
+++ src/conf/conf.js
@@ -0,0 +1,11 @@
+const conf = {
+    appwriteurl: String(import.meta.env.VITE_APPWRITE_UR),
+    appwriteprojetId: String(import.meta.env.VITE_PROJECT_ID),
+    appwriteDataaseIdId: String(import.meta.env.VITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_COLLECTION_ID),
+    appwriteBuckettId: String(import.meta.env.VITE_BUCKET_ID)
+}
+
+
+
+export default conf 
